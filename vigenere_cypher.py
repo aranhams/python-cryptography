e6b20fb53a29b52dbfadedcc2c9834543e46f54b
Matheus Aranha <aranha@MacBook-Air-de-Matheus.local>
2018-10-25 16:04:05 -0300

    correct some errors in the vigenere_cypher.py

--- vigenere_cypher.py
+++ vigenere_cypher.py
@@ -1,33 +1,33 @@
-def create_dict():
+def createdict_():
     return ({chr(y):y-65 for y in range(65,91)})
 
-def encrypt(_plaintxt=None, _key=None, _b=26, _dict=create_dict()):
+def encrypt(plaintxt_=None, key_=None, b_=26, dict_=createdict_()):
     ct = [] 
-    plaintxt = [_dict[i] for i in list(_plaintxt)]
-    key = [_dict[i] for i in list(_key)]
-    cyphertxt = [(plaintxt[i]+key[i%len(key)])%_b for i in range(len(plaintxt))]
+    plaintxt = [dict_[i] for i in list(plaintxt_)]
+    key = [dict_[i] for i in list(key_)]
+    cyphertxt = [(plaintxt[i]+key[i%len(key)])%b_ for i in range(len(plaintxt))]
 
     for i in cyphertxt:
-        for k, v in _dict.items():
+        for k, v in dict_.items():
             if v == i:
                 ct.append(k)
     return ''.join(ct)
 
-def decrypt(_cyphertxt=None, _key=None, _b=26, _dict=create_dict()):
+def decrypt(cyphertxt_=None, key_=None, b_=26, dict_=createdict_()):
     pt = [] 
-    cyphertxt = [_dict[i] for i in list(_cyphertxt)]
-    key = [_dict[i] for i in list(_key)]
-    plaintxt = [(cyphertxt[i]-key[i%len(key)])%_b for i in range(len(cyphertxt))]
+    cyphertxt = [dict_[i] for i in list(cyphertxt_)]
+    key = [dict_[i] for i in list(key_)]
+    plaintxt = [(cyphertxt[i]-key[i%len(key)])%b_ for i in range(len(cyphertxt))]
 
     for i in plaintxt:
-        for k, v in _dict.items():
+        for k, v in dict_.items():
             if v == i:
                 pt.append(k)
     return ''.join(pt)
 
 if __name__ == '__main__':
-    plaintxt = 'JOIN THE PARTY'
-    key = 'ABOUT'
+    plaintxt = 'CRYPTOGRAPHY'
+    key = 'LNCC'
 
     cyphertxt = encrypt(plaintxt.replace(" ",""), key)
     print(cyphertxt)
